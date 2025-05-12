34e505234d6303da0a3db50057a4eff362cb0bc7
Kushagra <kushagra100chhabra@gmail.com>
2025-05-12 19:12:31 +0530

    li

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -15,26 +15,29 @@ export default function Navbar() {
   return (
 
     <div  style={{ backgroundImage: 'url(https://t3.ftcdn.net/jpg/01/22/03/84/360_F_122038456_AEdKNITqNDwdPTVdPDy1Fk5YaD9Um1xm.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-      <div className='flex justify-center gap-20 text-white px-2 items-center py-10'>
-        {
-          <div className='text-4xl md:text-6xl items-center text-center px-27 md:px-2 '>
+      <div className='flex justify-between px-5 gap-25 text-white items-center py-10'>
+        <div>
+          {
+          <div className='text-4xl md:text-6xl items-center text-center px-20 md:px-2 '>
             EMPTY SPACES
           </div>
         }
-        {
+        </div>
+        <div className=' hidden md:flex justify-center gap-15'>
+          {
           data2.map((item, key) => (
-            <div className='hidden md:flex justify-between '>
-              <nav className=' items-center'>
-
-                <div className=' items-center flex justify-center '>
+            
+              <nav >
+                
                   <ul  >
                     <Link to={item.href}><li key={key} className='hover:border-b-4 hover:border-blue-500 '>{item.title1}</li></Link>
                   </ul>
-                </div>
+                
               </nav>
-            </div>
+            
           ))
         }
+        </div>
       </div>
 
 

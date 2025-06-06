5669f6d1cf8cea838b8cef278cb822b864dd29cd
Kushagra <kushagra100chhabra@gmail.com>
2025-06-07 00:10:47 +0530

    FE

--- vite-project/src/components/Season1.jsx
+++ vite-project/src/components/Season1.jsx
@@ -1,28 +1,128 @@
-import React from 'react'
+import React from 'react';
 
 export default function Season1() {
-    return (
-        <div className='  bg-black text-white'>
+  return (
+    <div className="bg-white text-gray-700 min-h-screen py-12 px-6 flex justify-center">
+      <main className="max-w-4xl w-full">
+        <h1 className="text-6xl font-extrabold text-center mb-8">
+          EMPTY SPACES
+        </h1>
 
-            <div className='text-6xl text-center py-10'>
-                EMPTY SPACES
-            </div>
+        <h2 className="text-4xl font-semibold text-center mb-12">
+          SEASON-1
+        </h2>
 
-            <div className=' py-5 text-center text-4xl'>
-                SEASON-1
-            </div>
+        <section className="bg-white rounded-xl shadow-md p-8 prose prose-lg max-w-none">
+          <h3 className="text-center font-bold mb-6 text-2xl">
+            EMPTY SPACES PART-1
+          </h3>
 
-            <div className=' px-5 '>
-                <p>
-                    <h2 className='text-center font-bold'>EMPTY SPACES PART-1</h2>
-                    <br /> <br />
-
-
-                    So in starting we see an interview is going on <br /> With the title ' The knowledge interview ' <br /> Let's we call the person taking interview A <br /> And the person giving it K <br /> So the interview starts <br />
-
-                    A : We welcome you and firstly , how are you? <br /><br /> K : Thanks , I am good and what about you <br /><br /> A : I am also good,, thanks btw for coming,, firstly tell how are you feeling , after reaching so much success in your life <br /><br /> K (little blushes) : thanks for the praise but success is not something it looks like <br /><br /> A : what do you mean <br /><br /> K : I mean one's success can be failure of another and may be success for any 3rd person <br /><br /> A : hmmm, can you tell how <br /><br /> K : I will explain you with an example ,, let 3 persons be A(not you) , B and C <br /> There is a competition <br /> Let A has power to defeat B but will loose from C <br /><br /> And first round is between B and C <br /><br /> Then then success of B is success and A and failure of C whereas <br /> Success success of C is failure of both A and B <br /><br /> A : hmmm nice ,, it means luck plays a major role ,, does it ? <br /><br /> K : yes luck always plays a role but it depends on you hardwork you did <br /><br /> A : how ,, I mean both are correlated,, it's weird to listen <br /><br /> K : yes , but listen how come <br /><br /> So <br /> Take a situation A,B and C <br /> A has done 50% hardwork <br /> Which means A can defeat C <br /> But it depends on luck whether he can fit C or not whereas B and C depends on luck if compete with each other <br /> And if once lost then out of the match <br /> Moreover depends on luck which 2 will first compete together <br /><br /> So if take cases then a total of 4 cases will come and and after adding then you will get probability of 3/4 which means of you have done 50% hardwork then there are 75% chance of your win <br /><br /> So this is how <br /><br /> Whereas if there are 4 persons and A has done 50% hardwork <br /> Which meant he can definitely defeat one of them <br /> Can defeat with probability 3/4 with 2nd of them <br /> And 1/2 probability with 3rd of them <br /><br /> So now there will be 15 cases and on adding them you get probability 75/96 which is around 78% <br /><br /> So now the 3rd case when there are 5 people <br /> You will definitely defeat 2 of them and other 2 depends on luck <br /> Then there will be 64 cases and on adding them you again get 3/4 which means 75 percent <br /><br /> So it's clear that if you have done 50% hardwork then chances of your win is 75%-80% <br /><br /> But remember this is when other players totally depend on luck when played with each other <br /> And when we see the cases in all then you will get to see that it depends on luck that you will win or not <br /><br /> So more hardwork more chance of your win but <br /> Remember luck will have no role of you have done 100% hardwork <br /><br /> A: oh damn , what an explanation, hope are viewers have understood it ,, so tell now do you consider yourself successful or not <br /><br /> K : not completely as I have to do many more <br /><br /> A : what else , you can done while you achieved so much ,, what's your goal then <br /><br /> K : I DON'T KNOW <br /><br /> A : hmmm , so can you tell how you manage all these things together and what should we call you,, <br /> (to viewers )- those who don't know K is a big youtuber , done many researchers in physics and maths , a good singer , and at last writes his own poetries , books and novels <br /><br /> K : I am youtuber by profession as from there I started to earn money <br /> Researches in science and maths are my passion , singing is something close to my heart , which I do for relaxation whereas I write in free spaces <br /><br /> A : what did you just said free spaces , i guess you mean free time <br /><br /> K : no , I mean free spaces only , but it's the time of day which is not empty, can be used , but you mostly waste , it's not the time where you are relaxing , like travelling, bathing, time before getting sleep or something else. I mostly use that time to write <br /><br /> A: Hmmm , but I guess many people don't have this time on regular basis <br /><br /> K : If universe can have empty apeces then why not you <br /><br /> A : wait what ! What do you mean by that <br /><br /> K : it's just a hint I am giving to you and all the viewers ,, think by yourself,, why should I tell everything <br /><br /> A : Ohk ,, then can you tell about empty heart and empty feelings,, cause some people just sometimes feel empty without any reason <br /><br /> K : yes I know,, I also have gone through that face ,, when I was just 18 , for a large period of time,, at least once in a month I went through that empty feeling phase and at that time , the only thing I could do was to cry . It totally depends on you,, from what thing you get satisfaction,, do that ,, or just cry ,, so by this you would fastly get out of the situation <br /><br /> A : ohk ,, i guess it's enough for now ,, so let's end it ,, it was nice meeting you <br /><br /> K(smiles) : same here but wait ,, you asked out many questions from now let's play a game <br /><br /> A : oh wow ,, why not ,, which game <br /><br /> K : guess the golden ball <br /><br /> A : means <br /><br /> K(taking out 2 identical balls from pocket) : these are 2 balls ,, in 2 halves joint by magnet . <br /> One is golden and other is silver from inside ,, I will see what's in that and will push one ball further i.e according to me you should choose that ball for win ,, so you have to decide to choose the golden ball <br /><br /> A : oh that's a nice game ,, I will definitely try <br /> (Looking to camera) : you all also try to guess <br /><br /> K(seeing the balls from inside and pushing one of them ) : so choose <br /><br /> A(taking the unpushed ball) : oh that's silver one but I was expecting it to be golden one <br /><br /> K : Hmmm , I know what you were thinking,, <br /> You thought someone dumb will push silver ball further , you will choose the other ball, the smarter one will push golden one further to confuse you and you know it so you will choose pushed ball , the more smarter one will push silver ball to double confuse you and you think me to be that one so chose unpushed ball and I knew that what you were thinking about me so I pushed golden ball further and so you got tricked <br /><br /> A: damn bro ,, I am out of this game ,, let's end the interview <br /><br />
-
-                    So after this interview was over <br /> We see K going home but he was on a cycle <br /> On reaching outer gates he had to place thumb on a device to scan <br /> Further on reaching the main door of house he had to answer few questions to enter the house <br /><br />
+          <p>
+            So in starting we see an interview is going on with the title 'The knowledge interview'. Let's call the person taking the interview A and the person giving it K. So the interview starts.
+            <br /><br />
+            A: We welcome you and firstly, how are you?
+            <br /><br />
+            K: Thanks, I am good and what about you?
+            <br /><br />
+            A: I am also good, thanks by the way for coming. Firstly, tell how are you feeling after reaching so much success in your life?
+            <br /><br />
+            K (little blushes): Thanks for the praise, but success is not something it looks like.
+            <br /><br />
+            A: What do you mean?
+            <br /><br />
+            K: I mean one's success can be the failure of another and maybe success for any third person.
+            <br /><br />
+            A: Hmmm, can you tell how?
+            <br /><br />
+            K: I will explain with an example. Let 3 persons be A (not you), B, and C. There is a competition. Let A have the power to defeat B but will lose to C.
+            <br /><br />
+            And the first round is between B and C.
+            <br /><br />
+            Then the success of B is success and A and failure of C, whereas the success of C is the failure of both A and B.
+            <br /><br />
+            A: Hmmm nice, it means luck plays a major role, does it?
+            <br /><br />
+            K: Yes, luck always plays a role, but it depends on the hard work you did.
+            <br /><br />
+            A: How? I mean both are correlated, it's weird to listen.
+            <br /><br />
+            K: Yes, but listen to how it comes.
+            <br /><br />
+            So take a situation A, B, and C. A has done 50% hard work, which means A can defeat C. But it depends on luck whether he can defeat C or not, whereas B and C depend on luck if they compete with each other. And if one loses, then they are out of the match. Moreover, it depends on luck which 2 will first compete together.
+            <br /><br />
+            So if we take cases, then a total of 4 cases will come, and after adding them, you will get a probability of 3/4, which means if you have done 50% hard work, then there are 75% chances of your win.
+            <br /><br />
+            So this is how.
+            <br /><br />
+            Whereas if there are 4 persons and A has done 50% hard work, which means he can definitely defeat one of them, can defeat with a probability of 3/4 with the 2nd of them, and 1/2 probability with the 3rd of them.
+            <br /><br />
+            So now there will be 15 cases, and on adding them, you get a probability of 75/96, which is around 78%.
+            <br /><br />
+            So now the 3rd case when there are 5 people, you will definitely defeat 2 of them, and the other 2 depend on luck.
+            <br /><br />
+            Then there will be 64 cases, and on adding them, you again get 3/4, which means 75 percent.
+            <br /><br />
+            So it's clear that if you have done 50% hard work, then chances of your win are 75%-80%.
+            <br /><br />
+            But remember this is when other players totally depend on luck when played with each other.
+            <br /><br />
+            And when we see the cases in all, then you will get to see that it depends on luck whether you will win or not.
+            <br /><br />
+            So more hard work means more chance of your win, but remember luck will have no role if you have done 100% hard work.
+            <br /><br />
+            A: Oh damn, what an explanation. Hope our viewers have understood it. So tell now, do you consider yourself successful or not?
+            <br /><br />
+            K: Not completely, as I have to do many more.
+            <br /><br />
+            A: What else can you do while you achieved so much? What's your goal then?
+            <br /><br />
+            K: I DON'T KNOW.
+            <br /><br />
+            A: Hmmm, so can you tell how you manage all these things together and what should we call you?
+            <br /><br />
+            (to viewers) - Those who don't know, K is a big YouTuber, has done many researches in physics and maths, a good singer, and at last writes his own poetries, books, and novels.
+            <br /><br />
+            K: I am a YouTuber by profession as from there I started to earn money. Researches in science and maths are my passion, singing is something close to my heart, which I do for relaxation, whereas I write in free spaces.
+            <br /><br />
+            A: What did you just say, free spaces? I guess you mean free time.
+            <br /><br />
+            K: No, I mean free spaces only, but it's the time of day which is not empty, can be used, but you mostly waste. It's not the time where you are relaxing, like traveling, bathing, time before getting sleep, or something else. I mostly use that time to write.
+            <br /><br />
+            A: Hmmm, but I guess many people don't have this time on a regular basis.
+            <br /><br />
+            K: If the universe can have empty spaces, then why not you?
+            <br /><br />
+            A: Wait, what! What do you mean by that?
+            <br /><br />
+            K: It's just a hint I am giving to you and all the viewers. Think for yourself. Why should I tell everything?
+            <br /><br />
+            A: Ohk, then can you tell about empty heart and empty feelings? Because some people just sometimes feel empty without any reason.
+            <br /><br />
+            K: Yes, I know. I also have gone through that phase. When I was just 18, for a large period of time, at least once a month, I went through that empty feeling phase, and at that time, the only thing I could do was cry. It totally depends on you, from what thing you get satisfaction, do that, or just cry, so by this, you would fastly get out of the situation.
+            <br /><br />
+            A: Ohk, I guess it's enough for now. So let's end it. It was nice meeting you.
+            <br /><br />
+            K (smiles): Same here, but wait, you asked out many questions from now. Let's play a game.
+            <br /><br />
+            A: Oh wow, why not? Which game?
+            <br /><br />
+            K: Guess the golden ball.
+            <br /><br />
+            A: Means?
+            <br /><br />
+            K (taking out 2 identical balls from pocket): These are 2 balls, in 2 halves joined by a magnet. One is golden and the other is silver from inside. I will see what's in that and will push one ball further, i.e., according to me, you should choose that ball for win, so you have to decide to choose the golden ball.
+            <br /><br />
+            A: Oh, that's a nice game. I will definitely try. (Looking at the camera) You all also try to guess.
+            <br /><br />
+            K (seeing the balls from inside and pushing one of them): So choose.
+            <br /><br />
+            A (taking the unpushed ball): Oh, that's the silver one, but I was expecting it to be the golden one.
+            <br /><br />
+            K: Hmmm, I know what you were thinking. You thought someone dumb will push the silver ball further, you will choose the other ball. The smarter one will push the golden one further to confuse you, and you know it, so you will choose the pushed ball. The more smarter one will push the silver ball to double confuse you, and you think me to be that one, so you chose the unpushed ball, and I knew what you were thinking about me, so I pushed the golden ball further, and so you got tricked.
+            <br /><br />
+            A: Damn bro, I am out of this game. Let's end the interview.
+            <br /><br />
+             So after this interview was over <br /> We see K going home but he was on a cycle <br /> On reaching outer gates he had to place thumb on a device to scan <br /> Further on reaching the main door of house he had to answer few questions to enter the house <br /><br />
 
                     1st question was <br /> there are how many types of universe/ multiverse <br /> He answered : 3 <br /><br />
 
@@ -2005,12 +2105,12 @@ export default function Season1() {
                 Scene 3 <br /><br />
 
                 K : My real fear is the INFINITY
+          </p>
 
-
-            </p>
-            
-
-        </div>
-        </div >
-    )
+       
+        </section>
+      </main>
+    </div>
+  );
 }
+
